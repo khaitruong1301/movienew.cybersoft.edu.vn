d3f7f9ffd4a7f84c4bb15345f1b975a58fd90586
phungnguyen9293 <phungnguyen9293@gmail.com>
2021-08-28 19:09:49 +0700

    dp

--- StatusConstants/StatusCodeConstants.cs
+++ StatusConstants/StatusCodeConstants.cs
@@ -21,6 +21,6 @@ namespace bookingticketAPI.StatusConstants
     public class Config
     {
         public static string hostName = "https://movieapinew.cybersoft.edu.vn/hinhanh/";
-        public static string connect = "Server=103.97.125.205,1433;database=dbRapChieuPhim;user id = khaicybersoft; password=khaicybersoft321@;Trusted_Connection=True;Integrated Security = False";
+        public static string connect = "Server=103.97.125.205,1433;database=dbmovienew;user id = khaicybersoft; password=khaicybersoft321@;Trusted_Connection=True;Integrated Security = False";
     }
 }
